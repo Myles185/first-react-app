24a24a6f08820667f736dafb75b9dfc2d01c70c2
Myles <mylesjohnson185@gmail.com>
2019-04-02 17:39:35 -0700

    Updates

---
+++ precache-manifest.235939c68fa13f1383349445930db920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/first-react-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  },
+  {
+    "revision": "33d041f62c201f2b77ef",
+    "url": "/first-react-app/static/js/main.8ec41982.chunk.js"
+  },
+  {
+    "revision": "ba0071fcdd781f148c8b",
+    "url": "/first-react-app/static/js/2.a94d1e15.chunk.js"
+  },
+  {
+    "revision": "33d041f62c201f2b77ef",
+    "url": "/first-react-app/static/css/main.825b1155.chunk.css"
+  },
+  {
+    "revision": "ba0071fcdd781f148c8b",
+    "url": "/first-react-app/static/css/2.e0b7bbd0.chunk.css"
+  },
+  {
+    "revision": "3a087b5b949909fc4f6258ac6357f709",
+    "url": "/first-react-app/index.html"
+  }
+];
